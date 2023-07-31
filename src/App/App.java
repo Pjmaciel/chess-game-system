a640846ad837f096d2275a78b37ca651eba16168
Pjmaciel <pjmaciel_4@hotmail.com>
2023-07-31 00:11:58 -0300

    Starting implement Board and Piece

---
+++ src/App/App.java
@@ -0,0 +1,13 @@
+package App;
+
+import BoardGame.Board;
+
+public class App {
+    public static void main(String[] args) {
+
+        Board board = new Board(8, 8);
+
+        System.out.println(board);
+
+    }
+}
